2ab8876b3a9a9e88182ddf97784523d79b63b394
Timo Tegtmeier <timo@tegi-stuff.de>
2010-08-29 19:55:43 +0200

    Final changes for release

---
+++ src/app/assistants/app-assistant.js
@@ -0,0 +1,274 @@
+/*
+ *		app/assistants/app-assistant.js
+ */
+
+/* FeedReader - A RSS Feed Aggregator for Palm WebOS
+ * Copyright (C) 2009, 2010 Timo Tegtmeier
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 3
+ * of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
+ */
+
+/**
+ * Global object that contains various global variables and some
+ * utility functions. It also contains the main model (feeds).
+ */
+FeedReader = {
+	appName:			"FeedReader",
+	appAuthor:			"Timo Tegtmeier",
+	versionString:		"2.0.1",
+	versionInt:			20,
+	copyrightYears:		"2009, 2010",
+
+	mainStageName: 		"FeedReaderStage",
+	dashboardStageName:	"FeedReaderDashboard",
+	
+	isActive: 			false,
+	showChangeLog:		false,
+	
+	feeds:				null,
+	prefs: 				null,
+	mediaExtensionLib:	null,
+	
+	controller:			{},
+	
+	/**
+	 * Show an alert.
+	 * 
+	 * @param {Template} 	message
+	 * @param {Hash} 		values
+	 */
+	showError: function(message, values) {
+		var appController = Mojo.Controller.getAppController();
+	    var cardStageController = appController.getStageController(this.mainStageName);
+		if (!cardStageController) {
+			return;
+		}
+		
+		var topScene = cardStageController.topScene();
+		if (!topScene) {
+			return;
+		}
+	
+		topScene.showAlertDialog({
+			title: $LL("Error"),
+			message: message.evaluate(values),
+			onChoose: undefined,
+			choices: [{
+				label: $LL("OK")
+			}]
+		});
+	},
+	
+	/**
+	 *
+	 * Send a SMS
+	 *
+	 * @param {String}		Body of the sms
+	 */
+	sendSMS: function(text) {
+		var req = new Mojo.Service.Request("palm://com.palm.applicationManager", {
+			   method: "open",
+			   parameters: {
+				   id: "com.palm.app.messaging",
+				   params: {
+					   messageText: text
+				   }
+			   }
+		});
+	},
+	
+	/**
+	 *
+	 * Send an E-Mail
+	 *
+	 * @param {String}		Subject line
+	 * @param {String}		Body of the E-Mail
+	 */
+	sendEMail: function(subject, text) {
+		var req = new Mojo.Service.Request("palm://com.palm.applicationManager", {
+			   method: "open",
+			   parameters:  {
+				   id: "com.palm.app.email",
+				   params: {
+					summary:	subject,
+					text:		text
+				}
+			}
+		});
+	},
+	
+	/**
+	 * Get media extension library.
+	 *
+	 * @return	{Object}			media extension library
+	 */
+	getMediaExtensionLib: function() {
+		if(!this.mediaExtensionLib) {
+			this.mediaExtensionLib = MojoLoader.require({
+				name: 		"mediaextension",
+				version:	"1.0"
+			});
+		}
+		return this.mediaExtensionLib;
+	}
+};
+
+/**
+ * Constructor for the AppAssistant.
+ * The AppAssistant has global lifetime.
+ * 
+ * @param {Object} appController
+ */
+function AppAssistant (appController) {
+	FeedReader.prefs = new prefs();
+	FeedReader.prefs.load();
+	FeedReader.feeds = new feeds();
+}
+
+/**
+ * Setup function for the AppAssistant.
+ */
+AppAssistant.prototype.setup = function() {	
+	FeedReader.prefs.setTimer();
+};
+
+/**
+ * Handle the app launch.
+ * 
+ * @param {Object} launchParams
+ */
+AppAssistant.prototype.handleLaunch = function (launchParams) {
+    Mojo.Log.info("ReLaunch requested");
+
+	FeedReader.controller = this.controller;
+    var cardStageController = this.controller.getStageController(FeedReader.mainStageName);
+    var appController = Mojo.Controller.getAppController();
+    
+    if (!launchParams) {
+        if (cardStageController) {
+			if(FeedReader.prefs.enableRotation) {
+				cardStageController.setWindowOrientation("free");
+			}
+            cardStageController.popScenesTo("feedlist");    
+            cardStageController.activate();
+        } else {
+            this.controller.createStageWithCallback({name: FeedReader.mainStageName, lightweight: true}, 
+                									function(stageController) {
+														stageController.enableManualSplashScreenMode();
+														if(FeedReader.prefs.enableRotation) {
+															stageController.setWindowOrientation("free");
+														}
+														stageController.pushScene("feedlist", FeedReader.feeds);
+													},
+													"card");
+        }
+    } else {
+        switch (launchParams.action) {
+			case "feedUpdate":
+				Mojo.Log.info("scheduled feed update");
+				FeedReader.prefs.setTimer();
+				if(FeedReader.feeds.isReady() && !FeedReader.feeds.isUpdating()) {
+					FeedReader.feeds.enqueueUpdateAll();
+				} else {
+					FeedReader.feeds.updateWhenReady = true;
+				}
+				break;
+        
+			case "bannerPressed":
+				if (cardStageController) {
+					if(FeedReader.prefs.enableRotation) {
+						cardStageController.setWindowOrientation("free");
+					}
+					if(cardStageController.topScene().sceneName != "feedlist") {
+						cardStageController.popScenesTo("feedlist");
+					}
+					cardStageController.activate();
+				} else {
+	                this.controller.createStageWithCallback({
+						name: FeedReader.mainStageName,
+						lightweight: true
+					}, function(stageController) {
+						stageController.enableManualSplashScreenMode();
+						if(FeedReader.prefs.enableRotation) {
+							stageController.setWindowOrientation("free");
+						}
+	                	stageController.pushScene("feedlist", FeedReader.feeds);
+	                }, "card");        
+	            }
+				break;
+		}
+	}
+};
+
+/**
+ * Handle the various menu commands.
+ * 
+ * @param {Object} event
+ */
+AppAssistant.prototype.handleCommand = function(event) {    
+    var stageController = this.controller.getActiveStageController();
+    var currentScene = stageController.activeScene();
+    
+    if (event.type == Mojo.Event.commandEnable) {
+        if (FeedReader.feeds.updateInProgress && (event.command == "do-feedUpdate")) {
+            event.preventDefault();
+        } else if((event.command == Mojo.Menu.helpCmd) || (event.command == Mojo.Menu.prefsCmd)) {
+			event.stopPropagation();
+		}
+    } else {
+        if(event.type == Mojo.Event.command) {
+            switch(event.command) {
+				case "do-import":
+					stageController.pushScene("import", FeedReader.feeds);
+					break;
+				
+                case "do-about":
+					var t = new Template($L("#{appName} — v#{version}"));
+					var m = new Template($L("Copyright #{years} #{author}, published under the terms of the GNU GPL v3. See License for details."));
+                    currentScene.showAlertDialog({
+                            onChoose: function(value) {},
+                            title:  t.evaluate({
+								appName: FeedReader.appName,
+								version: FeedReader.versionString
+							}), 
+                            message: m.evaluate({
+								years: FeedReader.copyrightYears,
+								author: FeedReader.appAuthor
+							}),
+                            choices:[{ label:$LL("OK"), value:"" }]});
+                	break;
+                
+				case "do-license":
+					stageController.pushScene("license");
+					break;
+				
+                case Mojo.Menu.prefsCmd:
+                    stageController.pushScene("preferences");
+                	break;
+                
+                case Mojo.Menu.helpCmd:
+                    stageController.pushAppSupportInfoScene();
+                	break;
+            
+                case "do-feedUpdate":
+                    FeedReader.feeds.enqueueUpdateAll();
+                	break;
+            }
+        }
+    }
+};
+
+AppAssistant.prototype.considerForNotification = function(params){
+};
---
+++ src/app/utils/spooler.js
@@ -0,0 +1,218 @@
+/*
+ *		app/models/spooler.js - activity spooler
+ */
+
+/* FeedReader - A RSS Feed Aggregator for Palm WebOS
+ * Copyright (C) 2009, 2010 Timo Tegtmeier
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 3
+ * of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
+ */
+
+var spooler = new Class.create({
+	list: [],
+	actionRunning: false,
+	actionIdent: "",
+	updateCounter: 0,
+	
+	/** @private
+	 *
+	 * Constructor.
+	 */
+	initialize: function() {
+		this.setActivitySuccessHandler = this.setActivitySuccess.bind(this);
+		this.setActivityFailedHandler = this.setActivityFailed.bind(this);
+		this.leaveActivitySuccessHandler = this.leaveActivitySuccess.bind(this);
+		this.leaveActivityFailedHandler = this.leaveActivityFailed.bind(this);
+	},
+	
+	/**
+	 * Enter update state. This will prevent the spooler from starting an
+	 * activity until the update state is left.
+	 */
+	beginUpdate: function() {
+		this.updateCounter++;
+	},
+	
+	/**
+	 * Leave update state. If actions were added while being in update state
+	 * the spooler will start the first action from the list.
+	 */
+	endUpdate: function() {
+		this.updateCounter--;
+		if(!this.actionRunning && (this.list.length >= 1)) {
+			DashboardControl.createUpdateDashboard();
+			Mojo.Controller.getAppController().sendToNotificationChain({ type: "updatestate-changed" });
+			this.nextAction();
+		}
+	},
+	
+	/**
+	 * Add an action to the spooler list.
+	 *
+	 * @param	action		{Function}		function to execute
+	 * @param	identifier	{String}		name of the action
+	 * @param	unique		{Boolean}		if true, only one action with the
+	 * 										given name is allowed to be executed
+	 * 										at any time
+	 */
+	addAction: function(action, identifier, unique) {
+		try {
+			if(!identifier) {
+				identifier = "anon-action";
+			}
+			
+			if(unique) {
+				var skip = false;
+				if(this.actionIdent == identifier) {
+					skip = true;
+				} else {
+					for(var i = 0; i < this.list.length; i++) {
+						if(this.list[i].ident == identifier) {
+							skip = true;
+							break;
+						}
+					}
+				}
+				
+				if(skip) {
+					return;
+				}
+			}
+			
+			this.list.push({
+				execute: action,
+				ident: identifier
+			});
+			
+			if((this.updateCounter === 0) && (this.list.length == 1) && !this.actionRunning)  {
+				DashboardControl.createUpdateDashboard();
+				Mojo.Controller.getAppController().sendToNotificationChain({ type: "updatestate-changed" });
+				this.nextAction();
+			}
+		} catch(e) {
+			Mojo.Log.logException(e);
+		}
+	},
+	
+	/** @private
+	 *
+	 * Tell the system, that we enter a phase of activity.
+	 */
+	enterActivity: function(duration) {
+		if(duration === undefined) {
+			duration = 60000;
+		}
+		var request = new Mojo.Service.Request("palm://com.palm.power/com/palm/power", {
+			method: "activityStart",
+			parameters: {
+				id: "com.tegi-stuff.app.feedreader",
+				duration_ms: duration
+			},
+			onSuccess: this.setActivitySuccessHandler,
+			onFailure: this.setActivityFailedHandler
+		});
+	},
+	
+	/** @private
+	 *
+	 * Gets called when setting the activity was successful.
+	 */
+	setActivitySuccess: function(response) {
+		Mojo.Log.info("SPOOLER> Successfully set activity");
+	},
+	
+	/** @private
+	 *
+	 * Gets called when setting the acitivity failed.
+	 */
+	setActivityFailed: function(response) {
+		Mojo.Log.error("SPOOLER> Unable to set activity", response);
+	},
+
+	/** @private
+	 *
+	 * Tell the system, that we left our activity phase.
+	 */
+	leaveActivity: function() {	
+		var request = new Mojo.Service.Request("palm://com.palm.power/com/palm/power", {
+			method: "activityEnd",
+			parameters: {
+				id: "com.tegi-stuff.app.feedreader"
+			},
+			onSuccess: this.leaveActivitySuccessHandler,
+			onFailure: this.leaveActivityFailedHandler
+		});
+	},
+
+	/** @private
+	 *
+	 * Gets called when setting the activity was successful.
+	 */
+	leaveActivitySuccess: function(response) {
+		Mojo.Log.info("SPOOLER> Successfully left activity");
+	},
+	
+	/** @private
+	 *
+	 * Gets called when setting the acitivity failed.
+	 */
+	leaveActivityFailed: function(response) {
+		Mojo.Log.error("SPOOLER> Unable to leave activity", response);
+	},
+	
+	/**
+	 * Called to indicate a spooled action has finished. The next spooled
+	 * activity will be started if any.
+	 */
+	nextAction: function() {
+		try {
+			if(this.list.length >= 1) {
+				var action = this.list.shift();
+				this.actionRunning = true;
+				this.actionIdent = action.ident;
+				this.enterActivity();
+				action.execute();
+			} else {
+				this.actionRunning = false;
+				this.actionIdent = "";
+				DashboardControl.removeUpdateDashboard();
+				FeedReader.feeds.interactiveUpdate = false;
+				Mojo.Controller.getAppController().sendToNotificationChain({ type: "updatestate-changed" });
+				this.leaveActivity();
+			}
+		} catch(e) {
+			Mojo.Log.logException(e);
+		}
+	},
+	
+	hasWork: function() {
+		return ((this.list.length > 0) ||
+				(this.actionRunning) ||
+				(this.updateCounter > 0));
+	},
+	
+	/**
+	 * Called to indicate that the app is about to be closed.
+	 */
+	aboutToClose: function() {
+		try {
+			if(this.hasWork()) {
+				DashboardControl.createUpdateDashboard(true);
+			}
+		} catch(e) {
+			Mojo.Log.logException(e);
+		}
+	}
+});
