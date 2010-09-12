ecb831f4105876a5a2997492f6aadb72429d8b31
Timo Tegtmeier <timo@tegi-stuff.de>
2010-09-12 19:05:48 +0200

    Changed version number to 2.0.2

--- src/app/assistants/app-assistant.js
+++ src/app/assistants/app-assistant.js
@@ -27,7 +27,7 @@
 FeedReader = {
 	appName:			"FeedReader",
 	appAuthor:			"Timo Tegtmeier",
-	versionString:		"2.0.1",
+	versionString:		"2.0.2",
 	versionInt:			20,
 	copyrightYears:		"2009, 2010",
 
