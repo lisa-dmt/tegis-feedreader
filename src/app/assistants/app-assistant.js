181a68d1a32315e85d763d8ce11a27364ca161cf
Timo Tegtmeier <Timo Tegtmeier@CUBE>
2011-06-04 14:01:52 +0200

    Bumped version to 2.1.3

--- src/app/assistants/app-assistant.js
+++ src/app/assistants/app-assistant.js
@@ -27,7 +27,7 @@
 FeedReader = {
 	appName:			"FeedReader",
 	appAuthor:			"Timo Tegtmeier",
-	versionString:		"2.1.2",
+	versionString:		"2.1.3",
 	versionInt:			21,
 	copyrightYears:		"2009-2011",
 
