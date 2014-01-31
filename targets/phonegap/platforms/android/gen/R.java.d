a913b042f45cf13f43a9fc3b4a49d1f21061e1dd
Timo Tegtmeier <timo@tegi-stuff.de>
2014-01-31 15:46:44 +0100

    Added phonegap files

---
+++ targets/phonegap/platforms/android/gen/R.java.d
@@ -0,0 +1,9 @@
+/home/timo/projects/FeedReader/src/targets/phonegap/platforms/android/gen/com/tegistuff/app/feedreader/R.java \
+ : /home/timo/projects/FeedReader/src/targets/phonegap/platforms/android/res/drawable-xhdpi/icon.png \
+/home/timo/projects/FeedReader/src/targets/phonegap/platforms/android/res/values/strings.xml \
+/home/timo/projects/FeedReader/src/targets/phonegap/platforms/android/res/drawable-ldpi/icon.png \
+/home/timo/projects/FeedReader/src/targets/phonegap/platforms/android/res/drawable/icon.png \
+/home/timo/projects/FeedReader/src/targets/phonegap/platforms/android/res/drawable-mdpi/icon.png \
+/home/timo/projects/FeedReader/src/targets/phonegap/platforms/android/res/drawable-hdpi/icon.png \
+/home/timo/projects/FeedReader/src/targets/phonegap/platforms/android/res/xml/config.xml \
+/home/timo/projects/FeedReader/src/targets/phonegap/platforms/android/bin/AndroidManifest.xml \
